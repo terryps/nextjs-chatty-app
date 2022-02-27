cf9caad41a72134fe545221348f6c3ba9e443852
terryps <jterry9702@gmail.com>
2022-02-27 22:52:40 +0900

    added error, loading jsx

--- components/home/FriendList.js
+++ components/home/FriendList.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { connect } from "react-redux";
+import Image from "next/image";
 
 const FriendList = ({ userId }) => {
     const [friendList, setFriendList] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [reload, setReload] = useState(false);
     const router = useRouter();
+
+    console.log("friend")
 
     useEffect(() => {
         setLoading(true);
@@ -23,21 +27,33 @@ const FriendList = ({ userId }) => {
             setFriendList(data.friendsData);
         }).catch(err => {
             setError(true);
+            setReload(false);
         }).finally(() => 
             setLoading(false)
         );
-    }, []);
+    }, [reload]);
 
     if(error) {
         return (
-            <div>
+            <div className="error">
+                <button onClick={()=>setReload(true)}>
+                    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                    </svg>
+                </button>
                 <span>Couldn't load friends list.</span>
             </div>
         )
     }
 
     else if(loading) {
-        return (<></>);
+        return (
+            <div className="loading">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        );
     }
 
     else {
@@ -46,9 +62,11 @@ const FriendList = ({ userId }) => {
                 <ul>
                 {
                     friendList.map(friend => 
-                        <li key={friend.id} className="flex-row lst-btn friend-lst-item trns-all" onClick={()=>router.push(`/${friend.username}`)}>
-                            <div className="lst-avatar"><span></span></div>
-                            <div className="flex-col lst-prof">
+                        <li key={friend.id} className="flex-row lst-item friend-lst-item trns-all" onClick={()=>router.push(`/${friend.username}`)}>
+                            <div className="avatar m-wd-72">
+                                <Image src="/static/avatars/5.png" width={48} height={48} />
+                            </div>
+                            <div className="lst-prof">
                                 <h3 className="ellipsis">{friend.username}</h3>
                                 <p className="ellipsis">{friend.fullname}</p>
                                 <span className="ellipsis">{friend.about}</span>
