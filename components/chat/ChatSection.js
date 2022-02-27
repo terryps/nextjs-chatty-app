c3b9bf4024969dffe56cd466aa1d7765179d126c
terryps <jterry9702@gmail.com>
2022-02-27 22:44:38 +0900

    added error modal && added error, loading jsx

--- components/chat/ChatSection.js
+++ components/chat/ChatSection.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { connect } from "react-redux";
 import TextForm from "./TextForm";
+import { Modal, MessageModal } from "components/modals/Modal";
 import { getTimeLapsed } from "lib/utils";
+import Image from "next/image";
 
 const ChatSection = ({ userId, hostInfo }) => {
     const [chatRoomId, setChatRoomId] = useState(null);
@@ -10,7 +12,9 @@ const ChatSection = ({ userId, hostInfo }) => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+    const [showError, setShowError] = useState(false);
     const router = useRouter();
+    console.log("====chat====");
 
     useEffect(() => {
         setLoading(true);
@@ -35,6 +39,11 @@ const ChatSection = ({ userId, hostInfo }) => {
         })
     }, []);
 
+    const _handleErrorModal = (open, message) => {
+        setShowError(open);
+        setErrorMessage(message);
+    }
+
     const handleLike = async (e, id, liked) => {
         e.preventDefault();
 
@@ -54,35 +63,54 @@ const ChatSection = ({ userId, hostInfo }) => {
                 )
             );
         }).catch(err => {
-            setErrorMessage(err);
+            _handleErrorModal(true, err);
         });
     }
 
     if(error) {
         return (
-            <div>
-                <h2>error</h2>
-                <span>Couldn't load messages.</span>
+            <div className="container">
+                <div className="chat-nav">
+                    <button onClick={()=>router.replace("/")} className="trns-all">
+                        <svg fill="none" stroke="#333" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                </div>
+                <div className="error">
+                    <button onClick={()=>router.reload()}>
+                        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                        </svg>
+                    </button>
+                    <span>Couldn't load messages.</span>
+                </div>
             </div>
         );
     }
 
     if(loading) {
         return (
-            <div>loading</div>
+            <div className="container">
+                <div className="loading">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
         )
     }
 
     return (
         <div className="container">
-            <div className="chat-nav flex-row">
+            <div className="chat-nav">
                 <button onClick={()=>router.push("/")} className="trns-all">
-                    <svg className="w-6 h-6" fill="none" stroke="#333" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg fill="none" stroke="#333" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                     </svg>
                 </button>
-                <div className="chat-nav-avatar">
-                    <span></span>
+                <div className="avatar m-wd-64">
+                    <Image src="/static/avatars/2.png" width={48} height={48} />
                 </div>
                 <div className="flex-col">
                     <h3>{hostInfo.username}</h3>
@@ -91,15 +119,15 @@ const ChatSection = ({ userId, hostInfo }) => {
             </div>
             
 
-            <div className="chat-container scroll-y flex-col-rev">
+            <div className="chat-container scroll-y">
                 <ul>
                     {
                         chatLog.map(el => (
                             <li key={el.id} className={`chat ${el.senderId===userId? "me" : "other"}`}>
                                 {
                                     el.senderId!==userId && 
-                                    <div className="chat-avatar">
-                                        <span></span>
+                                    <div className="avatar m-wd-56">
+                                        <Image src="/static/avatars/1.png" width={42} height={42} />
                                     </div>
                                 }
 
@@ -108,7 +136,7 @@ const ChatSection = ({ userId, hostInfo }) => {
                                     {
                                         el.senderId!==userId &&
                                         <button onClick={(e)=>handleLike(e,el.id,el.liked)} className="heart">
-                                            <svg className="w-6 h-6" fill={el.liked? "#D8565B":"none"} stroke={el.liked ? "#D8565B":"#333"} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <svg fill={el.liked? "#D8565B":"none"} stroke={el.liked ? "#D8565B":"#333"} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                             </svg>
                                         </button>
@@ -120,10 +148,18 @@ const ChatSection = ({ userId, hostInfo }) => {
                     }
                 </ul>
             </div>
+
             <TextForm
                 chatRoomId={chatRoomId}
                 updateLog={(chat) => setChatLog(chatLog.concat(chat))}
+                showError={(message)=>_handleErrorModal(true, message)}
             />
+
+            <Modal modalIsOpen={showError} handleClose={()=>_handleErrorModal(false, "")}>
+                <MessageModal type="Error" handleClose={()=>_handleErrorModal(false, "")}>
+                    { errorMessage }
+                </MessageModal>
+            </Modal>
         </div>
     );
 }
