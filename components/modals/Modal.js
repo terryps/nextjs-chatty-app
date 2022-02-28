3e9582cc6c33f09e510ef69292d1bed1fe1cd292
terryps <jterry9702@gmail.com>
2022-02-28 23:15:30 +0900

    made modals use redux store and added the profile edit modal

--- components/modals/Modal.js
+++ components/modals/Modal.js
@@ -1,46 +1,88 @@
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    hideMessageModal,
+    hideAddFriendModal,
+} from "redux/actions/modalActions";
 
-export const AddFriendModal = ({ userInfo, handleAdd, handleClose }) => {
+export const EditModal = () => {
     return (
-        <div className="flex-col modal-wrapper">
-            <div className="flex-col modal-content">
-                <Image className="modal-img" src="/static/avatars/1.png" width={48} height={48} />
-                <h3>{userInfo.username}</h3>
-                <p>{userInfo.fullname}</p>
+        <>
+            <div className="">
+                <form>
+                    <label for="fullname">name</label>
+                    <input type="text" id="fullname" name="fullname" value="fullname" maxLength={40} />
+
+                    <label for="username">username</label>
+                    <input type="text" id="username" name="username" value="username" maxLength={40} />
+
+                    <label>
+                        <textarea />
+                    </label>
+                </form>
             </div>
-            <button className="modal-btn ok-btn" onClick={handleAdd}><h3>add</h3></button>
-            <button className="modal-btn cancel-btn" onClick={handleClose}><h3>cancel</h3></button>
-        </div>
+        </>
     );
 }
 
-export const MessageModal = ({children, type, handleClose}) => {
+export const AddFriendModal = ({ handleAdd }) => {
+    const {open, userInfo} = useSelector(state=>state.addFriendModal);
+    const dispatch = useDispatch();
+    if(!open) { return <></>; }
     return (
-        <div className="flex-col modal-wrapper">
-            <div className="modal-content">
-                <h3>{type}</h3>
-                <p>{children}</p>
+        <>
+            <div className="modal-container">
+                <div className="flex-col modal-wrapper">
+                    <div className="flex-col modal-content">
+                        <Image className="modal-img" src="/static/avatars/1.png" width={48} height={48} />
+                        <h3>{userInfo.username}</h3>
+                        <p>{userInfo.fullname}</p>
+                    </div>
+                    <button
+                        className="modal-btn ok-btn"
+                        onClick={(e)=>{handleAdd(e);dispatch(hideAddFriendModal());}}
+                    >
+                            <h3>add</h3>
+                    </button>
+                    <button
+                        className="modal-btn cancel-btn"
+                        onClick={()=>dispatch(hideAddFriendModal())}
+                    >
+                        <h3>cancel</h3>
+                    </button>
+                </div>
             </div>
-            <button className="modal-btn ok-btn" onClick={handleClose}><h3>OK</h3></button>
-        </div>
+            <BackDrop onClick={()=>dispatch(hideAddFriendModal())} />
+        </>
     );
 }
 
-const BackDrop = ({onClick}) => {
-    return <div onClick={onClick} className="backdrop"></div>;
-  }
+export const MessageModal = () => {
+    const messageModal = useSelector(state=>state.messageModal);
+    const dispatch = useDispatch();
 
-export const Modal = ({ children, modalIsOpen, handleClose }) => {
-    if(!modalIsOpen) {
+    if(!messageModal.open) {
         return (<></>);
     }
 
     return (
         <>
             <div className="modal-container">
-                { children }
+                <div className="flex-col modal-wrapper">
+                    <div className="modal-content">
+                        <h3>{ messageModal.type }</h3>
+                        <p>{ messageModal.content }</p>
+                    </div>
+                    <button className="modal-btn ok-btn" onClick={()=>dispatch(hideMessageModal())}>
+                        <h3>OK</h3>
+                    </button>
+                </div>
             </div>
-            <BackDrop onClick={handleClose} />
+            <BackDrop onClick={()=>dispatch(hideMessageModal())} />
         </>
     );
+}
+
+const BackDrop = ({onClick}) => {
+    return <div onClick={onClick} className="backdrop"></div>;
 }
