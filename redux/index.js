10d00ca6c9e91749c35bbb7b13c3ab5a11c4594a
terryps <jterry9702@gmail.com>
2022-02-28 23:07:25 +0900

    added modal actions and states in reducer

--- redux/index.js
+++ redux/index.js
@@ -5,6 +5,10 @@ import {
     SET_LOGGED_IN,
     SET_USER_INFO,
     SET_HOST_INFO,
+    SHOW_MESSAGE_MODAL,
+    HIDE_MESSAGE_MODAL,
+    SHOW_ADD_FRIEND_MODAL,
+    HIDE_ADD_FRIEND_MODAL,
 } from "./actions/actionTypes";
 
 const initialState = {
@@ -12,6 +16,15 @@ const initialState = {
     isLoggedIn: false,
     userInfo: null,
     hostInfo: null,
+    messageModal: {
+        open: false,
+        type: "",
+        content: "",
+    },
+    addFriendModal: {
+        open: false,
+        userInfo: null,
+    },
 };
 
 const reducer = (state=initialState, action) => {
@@ -26,6 +39,14 @@ const reducer = (state=initialState, action) => {
             return { ...state, userInfo: action.payload, };
         case SET_HOST_INFO:
             return { ...state, hostInfo: action.payload, };
+        case SHOW_MESSAGE_MODAL:
+            return { ...state, messageModal: { open: true, ...action.payload, }, };
+        case HIDE_MESSAGE_MODAL:
+            return { ...state, messageModal: { open: false, type: "", content: "", }, };
+        case SHOW_ADD_FRIEND_MODAL:
+            return { ...state, addFriendModal: { open: true, ...action.payload, }, };
+        case HIDE_ADD_FRIEND_MODAL:
+            return { ...state, addFriendModal: { open: false, userInfo: null, }, };
         default:
             return state;
     }
