59975d0a0844981a907d196f3fbc7eb6c9b39f7e
terryps <jterry9702@gmail.com>
2022-02-28 23:14:08 +0900

    modified api/requests

--- pages/api/requests/index.js
+++ pages/api/requests/index.js
@@ -26,6 +26,7 @@ export default async function handler(req, res) {
                 
                 return res.status(200).json({ requestData: requesterData });
             } catch(err) {
+                console.log(err.message)
                 return res.status(500).json({ message: "Internal Server Error" });
             }
         default:
