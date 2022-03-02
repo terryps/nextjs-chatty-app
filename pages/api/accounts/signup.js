7ed4a0eb2d5b46ca49ace9d0b7b5ad44d9faa576
terryps <jterry9702@gmail.com>
2022-03-02 14:38:53 +0900

    modified to add parameters with spread op

--- pages/api/accounts/signup.js
+++ pages/api/accounts/signup.js
@@ -4,21 +4,25 @@ import { hash } from "bcrypt";
 export default async function handler(req, res) {
     switch (req.method) {
         case "POST":
+            const { fullname, username, password } = req.body;
             try {
+                if(username==="accounts") {
+                    return res.status(403).json({ message: `The username ${username} is not available.` });
+                }
                 const search = await prisma.user.findMany({
-                    where: { username: req.body.username, },
+                    where: { username: username, },
                 });
 
                 if(search?.length > 0) {
                     return res.status(409).json({ message: "This username isn't available. Please try another." });
                 }
 
-                const password = await hash(req.body.password, 10);
+                const passwordHash = await hash(password, 10);
                 const user = await prisma.user.create({
                     data: {
-                        fullname: req.body.fullname,
-                        username: req.body.username,
-                        password: password,
+                        fullname: fullname,
+                        username: username,
+                        password: passwordHash,
                     },
                 });
                 return res.status(200).json({ message: "Success" });
